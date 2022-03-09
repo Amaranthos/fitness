eff20c44caea7bdb8f3cbb2abc3926d2bb059aba
Joshua Hodkinson <joshua.hodkinson.42@gmail.com>
2022-03-09 11:02:37 +1000

    Add NotFound route

--- src/App.jsx
+++ src/App.jsx
@@ -11,10 +11,19 @@ export default () => (
         <Route index element={<Workouts />} />
         <Route path=":slug" element={<Workout />} />
       </Route>
+      <Route path="*" element={<NotFound />} />
     </Routes>
   </BrowserRouter>
 );
 
 function App() {
+  console.log("Rendering App");
+
   return <Workouts />;
+}
+
+function NotFound() {
+  console.log("Rendering NotFound");
+
+  return <p>Page not found</p>;
 }
